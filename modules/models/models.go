232444420038f7df1186878f307e8200e9458dc2
montree.p <montree.p@digitsoul.co.th>
2024-11-10 21:48:25 +0700

    add util and add add struct in models

--- modules/models/models.go
+++ modules/models/models.go
@@ -1,1 +1,29 @@
 package models
+
+type (
+	PaginateReq struct {
+		Start string `query:"start" validate:"max=64"`
+		Limit int    `query:"limit" validate:"min=2,max=20"`
+	}
+
+	PaginateRes struct {
+		Data  any           `json:"data"`
+		Limit int           `json:"limit"`
+		Tolal int64         `json:"total"`
+		First FirstPaginate `json:"first"`
+		Next  NextPaginate  `json:"last"`
+	}
+
+	FirstPaginate struct {
+		Herf  string `json:"herf"`
+		Start string `json:"start"`
+	}
+
+	NextPaginate struct {
+		Herf string `json:"herf"`
+	}
+
+	KafkaOffset struct {
+		Offset int64 `json:"offset" bson:"offset"`
+	}
+)
