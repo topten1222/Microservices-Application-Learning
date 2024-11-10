91c22b65cfe6ec0225bf030215fad5b67f3575bb
montree.p <montree.p@digitsoul.co.th>
2024-11-10 21:46:26 +0700

    add struct auth entity and auth models

--- modules/auth/authEntity.go
+++ modules/auth/authEntity.go
@@ -1,1 +1,25 @@
 package auth
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type (
+	Credential struct {
+		Id           primitive.ObjectID `json:"_id" bson:"_id,omitempty"`
+		PlayerId     string             `json:"player_id" bson:"player_id"`
+		RoleCode     int                `json:"role_code" bson:"role_code"`
+		AccessToken  string             `json:"access_token" bson:"access_token"`
+		RefreshToken string             `json:"refresh_token" bson:"refresh_token"`
+		CreatedAt    time.Time          `json:"created_at" bson:"created_at"`
+		UpdatedAt    time.Time          `json:"updated_at" bson:"updated_at"`
+	}
+
+	Role struct {
+		Id    primitive.ObjectID `json:"_id" bson:"_id,omitempty"`
+		Title string             `json:"title" bson:"title"`
+		Code  int                `json:"code" bson:"code"`
+	}
+)
