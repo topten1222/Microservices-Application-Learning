c1668234b4895c9a521f7e06e7265b4b4e4e5a41
montree.p <montree.p@digitsoul.co.th>
2024-11-10 21:47:41 +0700

    add struct player entity and player model

--- modules/player/playerModel.go
+++ modules/player/playerModel.go
@@ -1,1 +1,29 @@
 package player
+
+import "time"
+
+type (
+	PlayerProfile struct {
+		Id        string    `json:"_id"`
+		Email     string    `json:"email"`
+		Username  string    `json:"username"`
+		CreatedAt time.Time `json:"created_at"`
+		UpdatedAt time.Time `json:"updated_at"`
+	}
+
+	PlayerClaims struct {
+		Id       string `json:"_id"`
+		RoleCode int    `json:"role_code"`
+	}
+
+	CreatePlayerReq struct {
+		Email    string `json:"email" form:"email" validate:"required,email,max=255"`
+		Password string `json:"password" form:"password" validate:"required,max=30"`
+		Username string `json:"username" form:"username" validate:"required,max=64"`
+	}
+
+	CreatePlayerTransectionReq struct {
+		PlayerId string `json:"player_id" form:"player_id" validate:"required,max=64"`
+		Amount   int    `json:"amount" form:"amount" validate:"required"`
+	}
+)
