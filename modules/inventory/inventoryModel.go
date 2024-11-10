93708ca9a50ad1d99aca605c76cb8a902f85cab2
montree.p <montree.p@digitsoul.co.th>
2024-11-10 21:46:47 +0700

    add inventory entity and inventory model

--- modules/inventory/inventoryModel.go
+++ modules/inventory/inventoryModel.go
@@ -1,1 +1,23 @@
 package inventory
+
+import (
+	"github.com/topten1222/hello_sekai/modules/item"
+	"github.com/topten1222/hello_sekai/modules/models"
+)
+
+type (
+	UpdateInventoryReq struct {
+		PlayerId string `json:"player_id" validate:"required,max=64"`
+		ItemId   string `json:"item_id" validate:"required,max=64"`
+	}
+
+	ItemInventory struct {
+		InventoryId string `json:"inventory_id"`
+		*item.ItemShowCase
+	}
+
+	PlayerInventory struct {
+		PlayerId string `json:"player_id"`
+		*models.PaginateRes
+	}
+)
