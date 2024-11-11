ebaa749bf725173f7daef73a73ec7155b2c133c0
montree phromthong <montree.p@digitsoul.com>
2024-11-11 16:21:44 +0700

    add grpc inventory and item

--- modules/inventory/inventoryHandler/inventoryGrpcHandler.go
+++ modules/inventory/inventoryHandler/inventoryGrpcHandler.go
@@ -1,13 +1,23 @@
 package inventoryHandler
 
-import "github.com/topten1222/hello_sekai/modules/inventory/inventoryUsecase"
+import (
+	"context"
+
+	inventoryPb "github.com/topten1222/hello_sekai/modules/inventory/inventoryPb"
+	"github.com/topten1222/hello_sekai/modules/inventory/inventoryUsecase"
+)
 
 type (
 	inventoryGrpcHanlderService struct {
 		inventoryUsecase inventoryUsecase.InventoryUsecaseService
+		inventoryPb.UnimplementedInventoryGrpcServiceServer
 	}
 )
 
 func NewInventoryGrpcHandler(inventoryUsecase inventoryUsecase.InventoryUsecaseService) *inventoryGrpcHanlderService {
 	return &inventoryGrpcHanlderService{inventoryUsecase: inventoryUsecase}
+}
+
+func (g *inventoryGrpcHanlderService) IsAvailableToSell(ctx context.Context, req *inventoryPb.IsAvailableToSellToReq) (*inventoryPb.IsAvailableToSellToRes, error) {
+	return nil, nil
 }
--- modules/item/itemHandler/itemGrpcHandlerHandler.go
+++ modules/item/itemHandler/itemGrpcHandlerHandler.go
@@ -1,13 +1,23 @@
 package itemHandler
 
-import "github.com/topten1222/hello_sekai/modules/item/itemUsecase"
+import (
+	"context"
+
+	itemPb "github.com/topten1222/hello_sekai/modules/item/itemPb"
+	"github.com/topten1222/hello_sekai/modules/item/itemUsecase"
+)
 
 type (
 	itemGrpcHandler struct {
 		itemUsecase itemUsecase.ItemUsecaseService
+		itemPb.ItemGrpcServiceServer
 	}
 )
 
 func NewItemGrpcHandler(itemUsecase itemUsecase.ItemUsecaseService) *itemGrpcHandler {
 	return &itemGrpcHandler{itemUsecase: itemUsecase}
+}
+
+func (g *itemGrpcHandler) FindItemsInIds(ctx context.Context, req *itemPb.FindItemsInIdsReq) (*itemPb.FindItemsInIdsRes, error) {
+	return nil, nil
 }
